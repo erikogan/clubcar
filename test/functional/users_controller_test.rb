7804a0f41dec5f2f96edb94fa7377715af74748b
erik <erik>
2007-03-27 18:14:52 +0000

    adding session for a logged in user

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -11,45 +11,56 @@ class UsersControllerTest < Test::Unit::TestCase
     @controller = UsersController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
+    # there's a bug in the TestSession class, :user isn't found
+    @session	= { :user => users(:admin), 'user' => users(:admin) }
   end
 
   def test_should_get_index
-    get :index
+    get :index, {}, @session
     assert_response :success
     assert assigns(:users)
   end
 
   def test_should_get_new
-    get :new
+    get :new, {}, @session
     assert_response :success
   end
   
   def test_should_create_user
     old_count = User.count
-    post :create, :user => { }
+    user = { 
+      'name' => 'From Functional',
+      'login' => 'func'
+    }
+    user['plain_password'] = user['plain_password_confirmation'] = 'foo'
+
+    post :create, {:user => user}, { :user => users(:admin), 
+      # there's a bug in the TestSession class
+      'user' => users(:admin) }
+
     assert_equal old_count+1, User.count
     
     assert_redirected_to user_path(assigns(:user))
   end
 
   def test_should_show_user
-    get :show, :id => 1
+    get :show, { :id => 1}, @session
     assert_response :success
   end
 
   def test_should_get_edit
-    get :edit, :id => 1
+    get :edit, { :id => 1 }, @session
     assert_response :success
   end
   
   def test_should_update_user
-    put :update, :id => 1, :user => { }
+    put :update, { :id => 1, :user => { } }, @session
     assert_redirected_to user_path(assigns(:user))
   end
   
   def test_should_destroy_user
     old_count = User.count
-    delete :destroy, :id => 1
+    delete :destroy, { :id => 1 }, @session
     assert_equal old_count-1, User.count
     
     assert_redirected_to users_path
