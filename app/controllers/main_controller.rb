e6d23ce909ce31a906bdd468bea46ae0e96cdb6a
erik <erik>
2007-04-02 16:31:44 +0000

    fix the index when logging in

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -10,19 +10,8 @@ class MainController < ApplicationController
 private
 
   def find_user
-    @user_id = session[:user_id]
-    # ActiveRecord .find methods throw an exception when they fail, this
-    # needs to be reworked
-    begin
-      unless @user_id.blank?
-	@user = User.find(@user_id)
-	return
-      end
-    rescue
-      # The return handled the base case, everything else is redirected
-    end
-
-    redirect_to users_url
+    # It's not so much "find" anymore
+    @user = session[:user]
   end
 
 end
