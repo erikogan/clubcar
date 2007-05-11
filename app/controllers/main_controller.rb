dae576cb4865646447be90c404f46e6c635249c5
erik <erik>
2007-05-11 20:58:07 +0000

    mail warnings and fix who gets decision data

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -8,6 +8,18 @@ class MainController < ApplicationController
   end
 
 
+  def mail_warnings
+    users = User.find_all_by_present(false)
+    text = "<pre>\n"
+    users.each do |u|
+      email = ClubcarMailer.create_reactivate(u)
+      ClubcarMailer.deliver(email)
+      text += email.encoded + "\n--------------------------------------------------------------\n"
+    end
+    
+    render(:text => text + "\n</pre>")
+  end
+
   def inform_user
     @user = User.find(session[:user])
     email = ClubcarMailer.create_reactivate(@user) 
--- app/models/clubcar_mailer.rb
+++ app/models/clubcar_mailer.rb
@@ -1,18 +1,18 @@
 class ClubcarMailer < ActionMailer::Base
 
-FROM_ADDRESS = 'erik@cloudshield.com'
+FROM_ADDRESS = 'clubcar@eogan.usr.cloudshield.com'
 
   def reactivate(user)
     @subject    = '[clubcar] Lunch today?'
     @body       = {:user => user}
     @recipients = user.email.email
     @from       = FROM_ADDRESS
-    @sent_on    = Time.nowq
+    @sent_on    = Time.now
     @headers    = {}
   end
 
   def decision(choices)
-    users = User.find_all_by_present(false);
+    users = User.find_all_by_present(true);
 
     raise "No users are present!" if users.empty?
 
