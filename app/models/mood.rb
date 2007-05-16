171472fba99ffb55570c0ea9019cef310b148faa
erik <erik>
2007-05-16 13:47:10 +0000

    validation of the number of vetoes

--- app/models/mood.rb
+++ app/models/mood.rb
@@ -4,4 +4,18 @@ class Mood < ActiveRecord::Base
   has_many :restaurants, :through => :preferences
 
   attr_protected :active
+
+  VETO_MAX = 3
+
+  VETO_ID = Vote.find_by_name('Veto').id
+
+  def validate
+    # This looks at the SQL
+    #if (self.preferences.find_all_by_vote_id(VETO_ID).length > VETO_MAX)
+    # puts "WTF: #{self.preferences.select { |p| p.vote_id == VETO_ID }.length} : #{VETO_MAX} | #{VETO_ID}"
+    if (self.preferences.select { |p| p.vote_id == VETO_ID }.length > VETO_MAX)
+      errors.add_to_base("You are only allowed #{VETO_MAX} vetoes!")
+    end
+  end
+
 end
