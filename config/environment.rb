54a7d92a8312f9fd7acfed7de66f70d07849b21f
cloudshield <cloudshield>
2008-09-19 13:36:47 +0000

    roll back rails to 1.2.6 and everything works, crisis averted

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.1.1' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '1.2.6' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -27,10 +27,10 @@ Rails::Initializer.run do |config|
   # config.log_level = :debug
 
   # needed for 2.1
-  config.action_controller.session = { 
-    :session_key => "_myapp_session", 
-    :secret => "This is clubcar. It sucks, just ask Dwight." 
-  }
+  #config.action_controller.session = { 
+  #  :session_key => "_myapp_session", 
+  #  :secret => "This is clubcar. It sucks, just ask Dwight." 
+  #}
 
   # Use the database for sessions instead of the file system
   # (create the session table with 'rake db:sessions:create')
