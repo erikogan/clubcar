6887c5d8aba938ad094ed169a8a31a8142ae52f1
erik <erik>
2008-10-26 00:03:46 +0000

    use :senmail for mail. I bet this is why it stopped working for CS

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,7 @@ config.action_controller.perform_caching             = false
 config.action_view.cache_template_extensions         = false
 config.action_view.debug_rjs                         = true
 
-# Don't care if the mailer can't send
-# config.action_mailer.raise_delivery_errors = false
-# though right now, I do
+# Don't care if the mailer can't send (for now I do)
 config.action_mailer.raise_delivery_errors = true
+config.action_mailer.delivery_method = :sendmail
+#config.action_mailer.delivery_method = :test
