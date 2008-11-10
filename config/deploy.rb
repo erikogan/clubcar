77b4b25a8baec121322ff65a1c5d07a756ac69a7
Erik Ogan <erik@ogan.net>
2008-11-10 01:02:20 -0800

    Woo! deploy from git instead of SVN

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,5 @@
 set :application, "clubcar"
-set :repository,  "https://www.slackers.net/svn/erik/trunk/#{application}"
-
-set :scm_username, "cloudshield"
-# Until I get it installed, then we figure out how to get it to prompt
-set :scm_password, "cp0sRockz"
+set :repository,  "ssh://gomorrah.slackers.net/afs/slackers.net/projects/#{application}/repository"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
@@ -12,14 +8,15 @@ set :deploy_to, "/afs/slackers.net/projects/#{application}/release"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
-# set :scm, :subversion
+set :scm, :git
+set :branch, 'master'
 
 #role :app, "your app-server here"
 #role :web, "your web-server here"
 #role :db,  "your db-server here", :primary => true
 
 #server "clubcar.app.cloudshield.com", :app, :web, :db, :primary => true
-server "10.23.41.5", :app, :web, :db, :primary => true
+server "sarnath.slackers.net", :app, :web, :db, :primary => true
 
 # For now, we'll fix it later
 set :user, "erik"
