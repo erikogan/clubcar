7f6ed480398bb0e853b0525a91a306f05e229e65
Erik Ogan <erik@ogan.net>
2008-12-13 23:53:39 -0800

    NiftyCube rounded corners

--- public/javascripts/preferences.js
+++ public/javascripts/preferences.js
@@ -1,6 +1,7 @@
 function finish_save(basename) {
   saved = basename + "_saved"
   $(basename + "_spinner").hide()
+  Nifty("#" + saved, "small transparent")
   new Effect.Appear(saved, {duration: 0.3});
   new Effect.Fade(saved, {duration: 0.3, delay: 5.3});
   new Effect.Highlight(saved)
