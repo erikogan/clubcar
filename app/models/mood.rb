3630c2d389f439ba9d7b7d8a873d318aed671487
Erik Ogan <erik@ogan.net>
2009-06-07 00:20:38 -0700

    move the Vote fetch inside the method to make RSpec runs happier

--- app/models/mood.rb
+++ app/models/mood.rb
@@ -9,13 +9,13 @@ class Mood < ActiveRecord::Base
 
   VETO_MAX = 3
 
-  VETO_ID = Vote.find_by_name('Veto').id
-
   def validate
+    veto_id = Vote.find_by_name('Veto').id
+    
     # This looks at the SQL
     #if (self.preferences.find_all_by_vote_id(VETO_ID).length > VETO_MAX)
     # puts "WTF: #{self.preferences.select { |p| p.vote_id == VETO_ID }.length} : #{VETO_MAX} | #{VETO_ID}"
-    if (self.preferences.select { |p| p.vote_id == VETO_ID }.length > VETO_MAX)
+    if (self.preferences.select { |p| p.vote_id == veto_id }.length > VETO_MAX)
       errors.add_to_base("You are only allowed #{VETO_MAX} vetoes!")
     end
   end
