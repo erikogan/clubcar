8feb92295a25b953aa0c5d85133f7504f67a878f
Erik Ogan <erik@ogan.net>
2009-06-19 00:36:05 -0700

    New DB columns on users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,13 +3,16 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe User do
   before(:each) do
     @valid_attributes = {
-      :login => "value for login",
+      :login => "created",
       :name => "value for name",
-      :plain_password => "value for password",
-      :plain_password_confirmation => "value for password",
+      :password => "value for password",
+      :password_confirmation => "value for password",
       :salt => "value for salt",
       :present => false,
-      :admin => false
+      :admin => false,
+      :persistence_token => "created",
+      :perishable_token => "created",
+      :single_access_token => "created"
     }
   end
 
