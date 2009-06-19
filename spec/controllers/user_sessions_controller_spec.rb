9bbb02bc3a2c103414221ee16259395848ebd02c
Erik Ogan <erik@ogan.net>
2009-06-19 00:35:31 -0700

    remove extraneous tests for non-existent user_sessions_controller actions.

--- spec/controllers/user_sessions_controller_spec.rb
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -1,39 +1,17 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe UserSessionsController do
+  # should it really?
+  it_should_behave_like 'login'
 
   def mock_user_session(stubs={})
     @mock_user_session ||= mock_model(UserSession, stubs)
-  end
-  
-  describe "GET index" do
-    it "assigns all user_sessions as @user_sessions" do
-      UserSession.stub!(:find).with(:all).and_return([mock_user_session])
-      get :index
-      assigns[:user_sessions].should == [mock_user_session]
-    end
-  end
-
-  describe "GET show" do
-    it "assigns the requested user_session as @user_session" do
-      UserSession.stub!(:find).with("37").and_return(mock_user_session)
-      get :show, :id => "37"
-      assigns[:user_session].should equal(mock_user_session)
-    end
   end
 
   describe "GET new" do
     it "assigns a new user_session as @user_session" do
       UserSession.stub!(:new).and_return(mock_user_session)
       get :new
-      assigns[:user_session].should equal(mock_user_session)
-    end
-  end
-
-  describe "GET edit" do
-    it "assigns the requested user_session as @user_session" do
-      UserSession.stub!(:find).with("37").and_return(mock_user_session)
-      get :edit, :id => "37"
       assigns[:user_session].should equal(mock_user_session)
     end
   end
@@ -47,10 +25,10 @@ describe UserSessionsController do
         assigns[:user_session].should equal(mock_user_session)
       end
 
-      it "redirects to the created user_session" do
+      it "redirects to the root" do
         UserSession.stub!(:new).and_return(mock_user_session(:save => true))
         post :create, :user_session => {}
-        response.should redirect_to(user_session_url(mock_user_session))
+        response.should redirect_to(root_url)
       end
     end
     
@@ -67,51 +45,6 @@ describe UserSessionsController do
         response.should render_template('new')
       end
     end
-    
-  end
-
-  describe "PUT update" do
-    
-    describe "with valid params" do
-      it "updates the requested user_session" do
-        UserSession.should_receive(:find).with("37").and_return(mock_user_session)
-        mock_user_session.should_receive(:update_attributes).with({'these' => 'params'})
-        put :update, :id => "37", :user_session => {:these => 'params'}
-      end
-
-      it "assigns the requested user_session as @user_session" do
-        UserSession.stub!(:find).and_return(mock_user_session(:update_attributes => true))
-        put :update, :id => "1"
-        assigns[:user_session].should equal(mock_user_session)
-      end
-
-      it "redirects to the user_session" do
-        UserSession.stub!(:find).and_return(mock_user_session(:update_attributes => true))
-        put :update, :id => "1"
-        response.should redirect_to(user_session_url(mock_user_session))
-      end
-    end
-    
-    describe "with invalid params" do
-      it "updates the requested user_session" do
-        UserSession.should_receive(:find).with("37").and_return(mock_user_session)
-        mock_user_session.should_receive(:update_attributes).with({'these' => 'params'})
-        put :update, :id => "37", :user_session => {:these => 'params'}
-      end
-
-      it "assigns the user_session as @user_session" do
-        UserSession.stub!(:find).and_return(mock_user_session(:update_attributes => false))
-        put :update, :id => "1"
-        assigns[:user_session].should equal(mock_user_session)
-      end
-
-      it "re-renders the 'edit' template" do
-        UserSession.stub!(:find).and_return(mock_user_session(:update_attributes => false))
-        put :update, :id => "1"
-        response.should render_template('edit')
-      end
-    end
-    
   end
 
   describe "DELETE destroy" do
@@ -121,11 +54,10 @@ describe UserSessionsController do
       delete :destroy, :id => "37"
     end
   
-    it "redirects to the user_sessions list" do
+    it "redirects to the root" do
       UserSession.stub!(:find).and_return(mock_user_session(:destroy => true))
       delete :destroy, :id => "1"
-      response.should redirect_to(user_sessions_url)
+      response.should redirect_to(root_url)
     end
   end
-
 end
